6280645a985ffa7309eb786f9c161485bc121b1f
cyjhunnyboy <cyjhunnyboy@sina.com>
2019-10-03 22:27:17 -0400

    提交一个HelloWorld.java的文件

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+package per.cyj.demo;
+
+/**
+ * @author chenyongjun
+ * @apiNote HelloWorld Example
+ * @since 2019-10-04
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+    	System.out.println("Hello, World!");
+    }
+}
