3a91089e8a985b8a7cb737de194804f47ff71d09
cyjhunnyboy <32289806+cyjhunnyboy@users.noreply.github.com>
2019-10-04 10:47:54 +0800

    Update HelloWorld.java
    HelloWorld为主函数main添加JavaDoc注释

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,7 +6,11 @@ package per.cyj.demo;
  * @since 2019-10-04
  */
 public class HelloWorld {
-
+    
+    /**
+     * main函数是程序的入口
+     * @param args String[]数组参数
+     */
     public static void main(String[] args) {
     	System.out.println("Hello, World!");
     }
