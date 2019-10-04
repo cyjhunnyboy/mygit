246b570e7df100da3d09e0156cc6ccbf74273b17
cyjhunnyboy <cyjhunnyboy@sina.com>
2019-10-03 22:41:56 -0400

    新提交一个SayHello.java类

---
+++ SayHello.java
@@ -0,0 +1,17 @@
+package per.cyj.demo;
+
+/**
+ * @author chenyongjun
+ * @apiNote SayHello Demo
+ * @since 2019-10-04
+ */
+public class SayHello {
+
+    /**
+     * main函数，是程序的入口
+     * @param args String[]数组参数
+     */
+    public static void main(String[] args) {
+    	System.out.println("Hello, World!!!");
+    }
+}
